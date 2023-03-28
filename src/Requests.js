23e66a55453b97bdea612e61d35c8ba413832cbd
송가영 <song-gayeong@song-gayeong-ui-MacBookAir.local>
2023-03-28 11:51:20 +0900

    adding data(using tmdb) and creating main component

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = "44c5fd4ee0738d9a9868b3276aa52a97";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=ko&page=1&region=kr`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=ko&page=1&region=kr`,
+  requestTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}&language=ko&page=2&region=kr`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=ko&query=horror&page=1&include_adult=false&region=kr`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=ko&page=1&region=kr`,
+};
+
+export default requests;
