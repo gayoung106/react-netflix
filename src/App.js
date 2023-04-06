2ae91f21e311b3f31e45c2d42a2172a87aae594c
SongGayoung <xjongam1x@gmail.com>
2023-04-06 15:52:49 +0900

    modified error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import Nav from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthContextProvider } from "./context/AuthContext";
-
-import Favorite from "./pages/Favorite";
 import Home from "./pages/Home";
-
 import Login from "./pages/Login";
-import Movie from "./pages/Movie";
-import Recommendation from "./pages/Recommendation";
-import Series from "./pages/Series";
 import Signup from "./pages/Signup";
-import Trend from "./pages/Trend";
 import Pick from "./pages/Pick";
 import Navbar from "./components/Navbar";
 
